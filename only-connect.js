7d261e106492257d0a2aea6fbb648eef39c88001
saozneg <101975998+saozneg@users.noreply.github.com>
2022-05-28 22:17:35 +0200

    Update only-connect.js

--- only-connect.js
+++ only-connect.js
@@ -83,6 +83,7 @@
 				document.getElementById('modal').style.display = "none"
 				timer.play();
 			modal.style.height = wall.offsetHeight+"px";
+			modal.style.fontSize = (wall.offsetHeight/16)+"px";
 			};
 		};
 		
